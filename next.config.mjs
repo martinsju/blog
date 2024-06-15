d9953fe07ecd782d004e1adeddc12ecc03a78841
julia <agramartinsj@gmail.com>
2024-06-15 17:22:43 -0300

    build: build configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: ['images.unsplash.com', 'miro.medium.com'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
